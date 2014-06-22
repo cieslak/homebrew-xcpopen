0a9bb77395b57820d5b83b9bbc721f580dd6e9d6
Chris Cieslak <chris@electropuf.com>
2014-06-22 15:02:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+EPFxcpopen
+==========
+
+Tool to open Xcode projects from the command line.
+
+Options:
+
+    --help -h           Print this help message.
+    --directory -d path Open projects in this directory.
+    --enumerate -e      Deeply enumerate through directory and open all projects.
+    --xcode -x path     Open projects using Xcode path supplied.
---
+++ xcpopen.rb
@@ -0,0 +1,36 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Xcpopen < Formula
+  homepage "https://github.com/cieslak/EPFxcpopen"
+  sha1 "bf0a9cdf8e09126b52a88ae3dad9e53bf50d31a3"
+  url "https://dl.dropboxusercontent.com/u/41196486/xcpopen-0.1.tar.gz"
+ 
+# depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    # system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install", "DESTDIR=#{prefix}" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test xcpopen`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
